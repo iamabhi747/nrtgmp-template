7b8a4eb1022aa52837c0a2e78ae747ed33d6cc77
Abhinandan Bhuse <i.am.abhi747@gmail.com>
2024-12-04 11:15:31 +0530

    Extended Graphql setup to use moduler types & resolvers. resolvers format: src/graphql/**/*.resolver.js typedef format: src/grapgql/**/*.graphql

--- src/app/graphql/route.js
+++ src/app/graphql/route.js
@@ -1,24 +1,14 @@
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
 import { ApolloServer } from "@apollo/server";
+import { mergeResolvers, mergeTypeDefs } from '@graphql-tools/merge';
+import { loadFilesSync } from '@graphql-tools/load-files';
 
-const typeDefs=  `#graphql
-    type User { 
-        id: Int!
-        name: String!
-    }
-    
-    type Query {  
-        users: [User!]
-    }
-`;
 
-const resolvers = { 
-    Query: {
-        users: async (parent, args) => {
-            return [{ id: 1, name: "John Doe" }]; 
-        }
-    }
-}
+const resolversArray = loadFilesSync('src/graphql/**/*.resolver.js');
+const resolvers      = mergeResolvers(resolversArray);
+
+const typeDefsArray  = loadFilesSync('src/graphql/**/*.graphql');
+const typeDefs       = mergeTypeDefs(typeDefsArray);
 
 const server = new ApolloServer({ typeDefs, resolvers });
 const handler = startServerAndCreateNextHandler(server);
