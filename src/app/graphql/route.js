44be51cdb9ab02190e0e7145642eb4d7d21f1ba2
Abhinandan Bhuse <i.am.abhi747@gmail.com>
2024-12-05 17:20:20 +0530

    modular MongoDB setup added support for datasources

--- src/app/graphql/route.js
+++ src/app/graphql/route.js
@@ -2,6 +2,8 @@ import { startServerAndCreateNextHandler } from "@as-integrations/next";
 import { ApolloServer } from "@apollo/server";
 import { mergeResolvers, mergeTypeDefs } from '@graphql-tools/merge';
 import { loadFilesSync } from '@graphql-tools/load-files';
+import { mongoConnect } from "@/mongodb/connection";
+import { mongoDatasources } from "@/mongodb/datasources";
 
 
 const resolversArray = loadFilesSync('src/graphql/**/*.resolver.js');
@@ -10,7 +12,14 @@ const resolvers      = mergeResolvers(resolversArray);
 const typeDefsArray  = loadFilesSync('src/graphql/**/*.graphql');
 const typeDefs       = mergeTypeDefs(typeDefsArray);
 
-const server = new ApolloServer({ typeDefs, resolvers });
-const handler = startServerAndCreateNextHandler(server);
+await mongoConnect();
+
+const server = new ApolloServer({ typeDefs, resolvers});
+const handler = startServerAndCreateNextHandler(server, {
+    context: async (req, res) => ({
+        req, res,
+        ds: mongoDatasources
+    })
+});
 
 export { handler as GET, handler as POST };
